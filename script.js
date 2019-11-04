7dec3b7f55561db956405e7905c31841627ff4b2
surbhipachnanda <surbhi.pachnanda@gmail.com>
2019-11-04 14:33:18 +0530

    Shopping cart added

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 var submitted = false;
-
-$(".menu-items").slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    nextArrow: $(".next"),
-    prevArrow: $(".prev")
-});
-$(".menu-items1").slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    nextArrow: $(".next1"),
-    prevArrow: $(".prev1")
-});
-$(".menu-items2").slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    nextArrow: $(".next2"),
-    prevArrow: $(".prev2")
+$(document).ready(function() {
+    $(".menu-items").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        nextArrow: $(".next"),
+        prevArrow: $(".prev")
+    });
+    $(".menu-items1").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        nextArrow: $(".next1"),
+        prevArrow: $(".prev1")
+    });
+    $(".menu-items2").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        nextArrow: $(".next2"),
+        prevArrow: $(".prev2")
+    });
 });
